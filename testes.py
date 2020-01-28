22f4519150da6dfeacb68be683cc1ee1f7fe4ffe
Matthew Begun <matthew.begun@gmail.com>
2020-01-28 12:51:22 +1100

    Create testes.py

---
+++ testes.py
@@ -0,0 +1,8 @@
+# obligatory first code cell
+from pyforest import *
+from IPython.core.display import display, HTML
+display(HTML("<style>.container { width:85% !important; }</style>"))
+bam.enable()
+
+
+df = pd.read_csv('top-trumps-last-jedi.csv');df
