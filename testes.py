b5be11c57d640c52fb10f6720529b41dceed08d3
Matthew Begun <matthew.begun@gmail.com>
2020-02-01 14:47:40 +1100

    config

--- testes.py
+++ testes.py
@@ -1,8 +1,10 @@
 # obligatory first code cell
-from pyforest import *
+# from pyforest import *
+import pyforest
 from IPython.core.display import display, HTML
+
 display(HTML("<style>.container { width:85% !important; }</style>"))
 bam.enable()
 
-
-df = pd.read_csv('top-trumps-last-jedi.csv');df
+df = pd.read_csv("top-trumps-last-jedi.csv")
+df
