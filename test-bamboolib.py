f5f7c4d8936308d75b4870e473f22164e4846f9f
Matthew Begun <matthew.begun@gmail.com>
2020-02-03 17:03:08 +1100

    bamboo dammit

---
+++ test-bamboolib.py
@@ -0,0 +1,13 @@
+import pandas as pd
+# ^^^ pyforest auto-imports - don't write above this line
+# import bamboolib as bam
+# import pandas as pd
+
+# import some data
+df = pd.read_csv(bam.titanic_csv)
+
+# show the data
+df
+
+
+import pandas;print(pandas.__file__)
