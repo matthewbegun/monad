3cd0e4ee0299a7fe3e83027bcdc996665afb9155
Matthew Begun <matthew.begun@gmail.com>
2020-02-04 00:03:08 +1100

    churn

--- testes-trumps.py
+++ testes-trumps.py
@@ -1,3 +1,5 @@
+import seaborn as sns
+# ^^^ pyforest auto-imports - don't write above this line
 # obligatory first code cell
 # import pyforest
 from IPython.core.display import display, HTML
@@ -8,3 +10,17 @@ display(HTML("<style>.container { width:85% !important; }</style>"))
 # testes
 df = pd.read_csv("top-trumps-last-jedi.csv")
 df
+
+
+
+# pair plot
+g = sns.pairplot(df)
+
+g = sns.PairGrid(df)
+g = g.map_upper(sns.scatterplot)
+g = g.map_lower(sns.kdeplot)
+# g = g.map_diag(sns.kdeplot, lw=2)
+# g = g.map_diag(sns.rugplot, lw=2)
+g = g.map_diag(sns.distplot)
+
+
