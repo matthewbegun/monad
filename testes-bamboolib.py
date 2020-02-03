9a5fe6eea03a1c5ca281a8d2ffac6d73acfb10f7
Matthew Begun <matthew.begun@gmail.com>
2020-02-03 22:03:57 +1100

    whoops

---
+++ testes-bamboolib.py
@@ -0,0 +1,4 @@
+# import bamboolib as bam
+# import pandas as pd
+df = pd.read_csv(bam.titanic_csv)
+df
---
+++ testes-pyforest.py
@@ -0,0 +1,3 @@
+# testes-pyforest
+df = pd.DataFrame(dict(a=np.arange(10)))
+sns.distplot(df.a)
