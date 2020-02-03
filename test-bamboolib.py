3f8591488f0b1c160e7f60225554860ce91a3e17
Matthew Begun <matthew.begun@gmail.com>
2020-02-03 17:08:47 +1100

    fix up

--- test-bamboolib.py
+++ test-bamboolib.py
@@ -1,5 +1,3 @@
-import pandas as pd
-# ^^^ pyforest auto-imports - don't write above this line
 # import bamboolib as bam
 # import pandas as pd
 
@@ -8,6 +6,3 @@ df = pd.read_csv(bam.titanic_csv)
 
 # show the data
 df
-
-
-import pandas;print(pandas.__file__)
